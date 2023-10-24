727ca90e23a04f5957dce6da1e67e0a6efba04ee
Valerie Young <spectranaut@igalia.com>
2023-10-24 15:18:33 -0700

    Linux dependencies

--- lib/atspi/atspi_wrapper.cc
+++ lib/atspi/atspi_wrapper.cc
@@ -1,5 +1,5 @@
 #include <string>
-#include "lib/atspi/atspi.h"
+#include "lib/atspi/atspi_wrapper.h"
 
 std::string my_lib_function() {
     return "In library";
--- lib/atspi/dump_tree_atspi.cc
+++ lib/atspi/dump_tree_atspi.cc
@@ -1,12 +1,31 @@
-#include <string>
 #include <iostream>
 #include <ostream>
-#include "atspi.h"
+#include <regex>
+#include <string>
 
-int main() {
+#include <lib/atspi/atspi_wrapper.h>
 
-    std::cout << "Hello world!" << std::endl;
-    std::cout << my_lib_function() << std::endl;
+void print_usage(std::string& program_name) {
+    std::cout << "Usage: "<< program_name << " <pid>\n";
+}
+
+int main(int argc, char** argv) {
+    std::string program_name(argv[0]);
+
+    if (argc != 2) {
+        print_usage(program_name);
+        return 1;
+    }
+
+    std::string pid_string(argv[1]);
+    std::regex number_regex("\\d+");
+    if (!std::regex_match(pid_string, number_regex)) {
+        print_usage(program_name);
+        return 1;
+    }
+
+    const int pid = std::stoi(pid_string);
+    std::cout << "Got PID: " << pid << "\n";
 
     return 0;
 }
