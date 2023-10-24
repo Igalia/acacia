2120db77c84b78cf857fdd2a7a4adbc8562af99e
Alice Boxhall <95208+alice@users.noreply.github.com>
2023-10-24 16:01:31 +1100

    Added some super basic command line arg parsing for dump_tree_mac

--- lib/mac/dump_tree_mac.cc
+++ lib/mac/dump_tree_mac.cc
@@ -1,12 +1,32 @@
-#include <string>
+
 #include <iostream>
 #include <ostream>
+#include <regex>
+#include <string>
+
 #include "lib/mac/mac.h"
 
-int main() {
+void print_usage(std::string& program_name) {
+    std::cout << "Usage: "<< program_name << " <pid>\n";
+}
 
-    std::cout << "Hello world!" << std::endl;
-    std::cout << my_lib_function() << std::endl;
+int main(int argc, char** argv) {
+    std::string program_name(argv[0]);
+
+    if (argc != 2) {
+        print_usage(program_name);
+        return 1;
+    }
+
+    std::string pid_string(argv[1]);
+    std::regex number_regex("\\d+");
+    if (!std::regex_match(pid_string, number_regex)) {
+        print_usage(program_name);
+        return 1;
+    }
+
+    const int pid = std::stoi(pid_string);
+    std::cout << "Got PID: " << pid << "\n";
 
     return 0;
 }
