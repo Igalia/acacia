422137bad985a9988e88c876d2a474729e3e9788
Valerie Young <spectranaut@igalia.com>
2023-11-15 12:45:59 -0800

    Add example library use for nodejs

---
+++ examples/example-coreaam-tests.js
@@ -0,0 +1,137 @@
+/*
+`cooltest` is a wrapper around ava.
+
+In some other file, for this test suite, we would find the code that starts the browser, get the pid.
+
+`cooltest` will load the file passed in before getting the root accessible and passing it to the appropriate test (for example ATSPI, if running on linux). The root accessible is provided to the test in `t.context.rootAccessibe`.
+
+ */
+
+// Test of role
+// Spec: https://w3c.github.io/core-aam/#role-map-checkbox
+
+/* tests/checkbox.html
+ *
+ * <!DOCTYPE html>
+ * <html lang="en-US">
+ *     <div role='checkbox' id='test'>content</div>
+ * </html>
+ */
+
+cooltest({
+  file: 'tests/checkbox-mixed.html',
+  name: "role=checkbox",
+  ATSPI: async (t) => {
+
+    // *findAccessibleById* is from our library
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    // Alternatively, we might end up doing:
+    let checkboxByChild = t.context.rootAccessible().child().child()...;
+
+    // Or by name (not gaurenteed to be unique, so maybe returns list)
+    let checkboxByName = t.context.rootAccessible().findAccessibleByName('content')[0];
+
+    // checkbox == checkboxByChild == checkboxByName, and
+    // *findAccessibleById*, *child*, *findAccessibleByName* would be cross platform?
+
+    t.is(checkbox.atspi_accessible_get_role(), 'ROLE_CHECK_BOX');
+
+    // notes:
+    // *atspi_accessible_get_role* is exposed by our library and
+    // matches the ATSPI API.
+
+  },
+  AXAPI: async (t) => {
+
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    t.is(checkbox.AXRole, 'AXCheckBox');
+    t.is(checkbox.AXSubrole, '<nil>');
+
+    // notes:
+    // I'm not sure how important the different between `nil` and the empty string is.
+
+  },
+  IA2: async (t) => {
+
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    t.is(checkbox.get_accRole, 'ROLE_SYSTEM_CHECKBUTTON');
+    t.is(checkbox.queryInterfaceIA2.role, '');
+
+    // notes:
+    // IA2 has the concept of "interfaces" which have different information about the
+    // object. *QueryInterfaceIA2* does not match the API, it looks more like:
+    // https://learn.microsoft.com/en-us/windows/win32/api/unknwn/nf-unknwn-iunknown-queryinterface(refiid_void)
+  },
+  UIA: async (t) => {
+
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    t.is(checkbox.GetCachedPropertyValue('UIA_ControlTypePropertyId'), 'Checkbox')
+    // notes:
+    // I'm the least familiar with this API, I really don't know if this the part
+    // of the API we want to expose.
+  },
+});
+
+// Test of attribute aria-checked=true
+// Spec: https://w3c.github.io/core-aam/#ariaCheckedTrue
+
+/* tests/aria-checked-true.html
+ *
+ * <!DOCTYPE html>
+ * <html lang="en-US">
+ *     <div role='checkbox' id='test' aria-checked='true'>content</div>
+ * </html>
+ */
+
+cooltest({
+  file: 'tests/checkbox-mixed.html',
+  name: "role=checkbox",
+  ATSPI: async (t) => {
+
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    t.true(checkbox.atspi_state_set_get_states()['STATE_CHECKABLE'];
+    t.true(checkbox.atspi_state_set_get_states()['STATE_CHECKED']);
+
+    // notes:
+    // *atspi_state_set_get_states* is exposed by our library and
+    // matches the ATSPI API name. It returns an array of strings.
+
+  },
+  AXAPI: async (t) => {
+
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    t.is(checkbox.AXValue, 1);
+  },
+  IA2: async (t) => {
+
+    let checkbox = t.context.rootAccessible().findAccessibleById('test');
+
+    t.true(checkbox.get_accState()['STATE_SYSTEM_CHECKED']);
+    t.is(checkbox.queryInterfaceIA2.get_attributes()['checkable'], 'true');
+  },
+  UIA: async (t) => {
+
+    // TODO
+
+  },
+});
+
+
+// TODO
+// Test of accessible events when the aria-checked attribute changes
+// Spec: https://w3c.github.io/core-aam/#event-aria-checked
+
+/* tests/checkbox.html
+ *
+ * <!DOCTYPE html>
+ * <html lang="en-US">
+ *     <div role='checkbox' id='test' aria-checked='true'>content</div>
+ * </html>
+ */
+
