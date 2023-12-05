1ce40847ca14f6f73e228acb3539503fd9897511
Eduardo Lima <elima@igalia.com>
2023-12-05 14:56:49 +0100

    [ATSPI] Add various methods to AtspiNode to support tree traversal
    The new methods are:
    - accessible_get_child_count()
    - accessible_get_child_at_index()
    - accessible_get_children()
    
    The first two map directly to AT-SPI C library, the third is a
    convenient way of getting all children of a node at once.
    
    The `dump_tree_atspi` program is update to actually dump the
    accessible tree, using the newly added API.
    
    Fixes issue #20 <https://github.com/Igalia/AXAccess/issues/20>

--- include/axaccess/atspi/linux_utils.h
+++ include/axaccess/atspi/linux_utils.h
@@ -6,8 +6,6 @@
 
 #include "atspi_node.h"
 
-using AtspiNodePtr = std::unique_ptr<AtspiNode>;
-
 AtspiNodePtr find_root_accessible_from_pid(const int pid);
 
 #endif // LIB_ATSPI_LINUX_UTILS_H_
--- lib/atspi/dump_tree_atspi.cc
+++ lib/atspi/dump_tree_atspi.cc
@@ -9,6 +9,25 @@ void print_usage(std::string& program_name) {
     std::cout << "Usage: "<< program_name << " <pid>\n";
 }
 
+static void print_node(AtspiNodePtr& node, int level) {
+  for (auto i = 0; i < level; i++)
+    std::cout << "--";
+  std::cout << "> " << node->accessible_get_role_name();
+  std::string node_name = node->accessible_get_name();
+  if (!node_name.empty())
+    std::cout << " (" << node_name << ")";
+  std::cout << "\n";
+
+  int32_t child_count = node->accessible_get_child_count();
+  if (child_count < 0)
+    return;
+
+  for (auto i = 0; i < child_count; i++) {
+    auto child = node->accessible_get_child_at_index(i);
+    print_node(child, level + 1);
+  }
+}
+
 int main(int argc, char** argv) {
     std::string program_name(argv[0]);
 
@@ -27,8 +46,12 @@ int main(int argc, char** argv) {
     const int pid = std::stoi(pid_string);
     std::cout << "Got PID: " << pid << "\n";
     AtspiNodePtr root = find_root_accessible_from_pid(pid);
+    if (!root) {
+      std::cerr << "No accessible root found at pid " << pid << "\n";
+      return -1;
+    }
 
-    std::cout << "root accessible name: " << root->accessible_get_name() << "\n";
-    std::cout << "root accessible role: " << root->accessible_get_role_name() << "\n";
+    print_node(root, 0);
+
     return 0;
 }
