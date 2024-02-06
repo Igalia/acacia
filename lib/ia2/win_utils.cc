8bae7b79475ad625c2a4dd5773069fe63f2a21b9
Valerie Young <spectranaut@igalia.com>
2024-02-06 07:51:15 -0800

    Apply suggestions from alice
    Co-authored-by: Alice <95208+alice@users.noreply.github.com>

--- lib/ia2/win_utils.cc
+++ lib/ia2/win_utils.cc
@@ -6,7 +6,7 @@
 //#include <oleacc.h>
 //#include <wrl/client.h>
 
-// to do: we will probably need to get these from the IAccessible2 git repo, like in Chromium
+// TODO: we will probably need to get these from the IAccessible2 git repo, like in Chromium
 //#include "third_party/iaccessible2/ia2_api_all.h"
 
 // imports to try to get the hwnd
@@ -36,5 +36,7 @@ void GetAllWindowsFromProcessID(DWORD dwProcessID, std::vector<HWND> &hwnds)
 Ia2NodePtr find_root_accessible_from_pid(const int pid) {
     std::vector<HWND> hwnds;
     GetAllWindowsFromProcessID(pid, hwnds);
+    // TODO: Get the IAccessible2 pointer(s) for each HWND and construct an IA2Node for each
+    // TODO: Construct a top-level IA2Node with the collection of IA2Nodes as children
     return std::make_unique<Ia2Node>(Ia2Node());
 }
